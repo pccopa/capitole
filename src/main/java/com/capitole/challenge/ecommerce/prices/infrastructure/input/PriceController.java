a596af5d209773594b74d260ecea0818abbe425c
Pablo Copa <pablocopa@gmail.com>
2024-01-09 16:56:00 -0300

    Price controller added

---
+++ src/main/java/com/capitole/challenge/ecommerce/prices/infrastructure/input/PriceController.java
@@ -0,0 +1,27 @@
+package com.capitole.challenge.ecommerce.prices.infrastructure.input;
+
+import com.capitole.challenge.ecommerce.prices.application.ports.in.FindPriceByDateCommand;
+import com.capitole.challenge.ecommerce.prices.application.ports.in.FindProductPriceByDatePort;
+import com.capitole.challenge.ecommerce.prices.application.ports.out.PriceByDateResponse;
+import com.capitole.challenge.ecommerce.shared.WebAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@WebAdapter
+@RequestMapping ("v1/prices")
+@RequiredArgsConstructor
+public class PriceController {
+
+    private final FindProductPriceByDatePort findProductPriceByDatePort;
+
+    @GetMapping("/brands/{brandId}/products/{productId}")
+    public PriceByDateResponse getPricesByDate (@PathVariable Long brandId,
+                                                @PathVariable Long productId,
+                                                @RequestParam("searchDate") LocalDateTime searchDate) {
+        FindPriceByDateCommand command = new FindPriceByDateCommand(brandId, productId, searchDate);
+        return findProductPriceByDatePort.find(command);
+    }
+
+}
