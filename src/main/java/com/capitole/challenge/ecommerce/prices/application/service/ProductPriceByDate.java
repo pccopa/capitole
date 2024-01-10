88ad4aa4c62a434600f162aa8eb298bfc05e2212
Pablo Copa <pablocopa@gmail.com>
2024-01-10 11:01:18 -0300

    Use case to find correct price

--- src/main/java/com/capitole/challenge/ecommerce/prices/application/service/ProductPriceByDate.java
+++ src/main/java/com/capitole/challenge/ecommerce/prices/application/service/ProductPriceByDate.java
@@ -2,13 +2,25 @@ package com.capitole.challenge.ecommerce.prices.application.service;
 
 import com.capitole.challenge.ecommerce.prices.application.ports.in.FindPriceByDateCommand;
 import com.capitole.challenge.ecommerce.prices.application.ports.in.FindProductPriceByDatePort;
+import com.capitole.challenge.ecommerce.prices.application.ports.out.FindPricePort;
 import com.capitole.challenge.ecommerce.prices.application.ports.out.PriceByDateResponse;
+import com.capitole.challenge.ecommerce.prices.domain.model.Price;
 import com.capitole.challenge.ecommerce.shared.UseCase;
+import lombok.RequiredArgsConstructor;
 
 @UseCase
+@RequiredArgsConstructor
 public class ProductPriceByDate implements FindProductPriceByDatePort {
+
+    private final FindPricePort pricePort;
+
     @Override
     public PriceByDateResponse find(FindPriceByDateCommand command) {
-        return null;
+        Price price = pricePort.findPriceWithPriority(command.getBrandId(), command.getProductId(), command.getSearchDate());
+        return new PriceByDateResponse(command.getProductId(),
+                command.getBrandId(),
+                price.getPriceList(),
+                command.getSearchDate(),
+                price.getPrice());
     }
 }
