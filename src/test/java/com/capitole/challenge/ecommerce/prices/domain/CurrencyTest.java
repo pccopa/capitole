0b7f0676c25d2b533b5b354c203b0d553c09b22c
Pablo Copa <pablocopa@gmail.com>
2024-01-10 14:36:08 -0300

    Price and currency unit test

--- src/test/java/com/capitole/challenge/ecommerce/prices/domain/CurrencyTest.java
+++ src/test/java/com/capitole/challenge/ecommerce/prices/domain/CurrencyTest.java
@@ -1,4 +1,26 @@
 package com.capitole.challenge.ecommerce.prices.domain;
 
+import com.capitole.challenge.ecommerce.prices.domain.exceptions.InvalidCurrencyException;
+import com.capitole.challenge.ecommerce.prices.domain.model.Currency;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CurrencyTest {
+
+    @Test
+    public void validCurrencyName () {
+        assertDoesNotThrow(()-> {
+            Currency.create("EUR");
+        });
+    }
+
+    @Test
+    public void inValidCurrencyName () {
+        assertThrows(InvalidCurrencyException.class, ()-> {
+            Currency.create("asdf123");
+        });
+    }
+
 }
--- src/test/java/com/capitole/challenge/ecommerce/prices/domain/PriceTest.java
+++ src/test/java/com/capitole/challenge/ecommerce/prices/domain/PriceTest.java
@@ -1,4 +1,61 @@
 package com.capitole.challenge.ecommerce.prices.domain;
 
+import com.capitole.challenge.ecommerce.prices.domain.model.Currency;
+import com.capitole.challenge.ecommerce.prices.domain.model.Price;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PriceTest {
+
+    @Test
+    public void dateBetweenPriceDates () {
+        Price price = new Price();
+        var fiveDaysAgo = LocalDateTime.now().minusDays(5);
+        var tomorrow = LocalDateTime.now().plusDays(1);
+        var searchDate = LocalDateTime.now();
+        price.setEndDate(tomorrow);
+        price.setStartDate(fiveDaysAgo);
+
+        assertTrue(price.isBetween(searchDate));
+    }
+
+
+    @Test
+    public void dateBeforePriceDates () {
+        Price price = new Price();
+        var inFiveDays = LocalDateTime.now().plusDays(5);
+        var tomorrow = LocalDateTime.now().plusDays(1);
+        var searchDate = LocalDateTime.now();
+        price.setEndDate(tomorrow);
+        price.setStartDate(inFiveDays);
+
+        assertFalse(price.isBetween(searchDate));
+    }
+
+    @Test
+    public void dateAfterPriceDates () {
+        Price price = new Price();
+        var fiveDaysAgo = LocalDateTime.now().minusDays(5);
+        var tomorrow = LocalDateTime.now().minusDays(1);
+        var searchDate = LocalDateTime.now();
+        price.setEndDate(tomorrow);
+        price.setStartDate(fiveDaysAgo);
+
+        assertFalse(price.isBetween(searchDate));
+    }
+
+
+    @Test
+    public void getCurrencyName () {
+        Currency curr = Currency.create("EUR");
+        Price price = new Price();
+        price.setCurrency(curr);
+
+        assertEquals("EUR", price.getCurrencyName());
+    }
+
+
 }
